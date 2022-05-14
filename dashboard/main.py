c52794976789846379d56eaddc0ba0e9f9ecc44b
Kira-Floris <nzafloris@gmail.com>
2022-05-14 13:15:37 +0200

    dashboard creating dashboard

--- dashboard/main.py
+++ dashboard/main.py
@@ -3,6 +3,15 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 import plotly.figure_factory as ff
 import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from plot_relations import plot_relations
+
+# imports datasets
+clean_data = pd.read_csv('../data/clean_data.csv')
+user_data = pd.read_csv('../data/all_user_data.csv')
+experience_data = pd.read_csv('../data/experience_data.csv')
 
 
 # project description
@@ -11,7 +20,59 @@ def description_function():
 
 # data analysis
 def analysis_function():
-	pass
+	# task 1
+	st.header('Task 1')
+	# dataset in whole and columns used in it
+	st.subheader('Dataset in whole(Sample of 100 values)')
+	st.dataframe(clean_data[:100])
+
+	# aggregating per user using msind
+	st.subheader('Agregating per user information(Sample of 100 Users')
+	st.dataframe(user_data[:100])
+
+	# mean median describe and brief description
+	st.subheader('Statistical description of data')
+	st.dataframe(user_data.describe())
+
+	# total dl and ul vs applications
+	st.subheader('Total download and upload bytes vs applications')
+	
+	plot_list = ['Social Media DL (Bytes)', 'Social Media UL (Bytes)','Google DL (Bytes)','Google UL (Bytes)','Email DL (Bytes)','Email UL (Bytes)','Youtube DL (Bytes)','Youtube UL (Bytes)','Netflix DL (Bytes)','Netflix UL (Bytes)','Gaming DL (Bytes)', 'Gaming UL (Bytes)']
+	view_plots = st.multiselect('Choose plots to view',
+			plot_list
+		)
+	
+	sample_size = st.slider('Sample Size', 10, int(len(clean_data)))
+	for figure in view_plots:
+		main = figure
+		plot = plot_relations(clean_data[['Dur. (ms).1','Dur. (ms)','Total DL (Bytes)','Total UL (Bytes)',main]][:sample_size], main)
+		st.pyplot(plot)
+	# mean of deciles
+
+
+	# task 2
+	st.header('Task 2')
+	# engagement clusters graph and experience
+	st.subheader('Engagement Clusters graph, Duration vs Total DL')
+	fig = px.scatter(user_data,x='Dur. (ms)',y='Total DL (Bytes)',color='Cluster Engagement', log_x=True, size_max=60)
+	fig.update_layout(width=800)
+	st.write(fig)
+	# top 3 most used applications
+
+	# task 3
+	# experience clusters graph
+	st.subheader('Experience Clusters graph, Avg RTT DL (ms) vs DL TP < 50 Kbps (%)')
+	fig = px.scatter(user_data,x='Avg RTT DL (ms)',y='DL TP < 50 Kbps (%)',color='Cluster Experience', log_x=True, size_max=60)
+	fig.update_layout(width=800)
+	st.write(fig)
+
+	# task 4
+	st.header('Task 4')
+	# top 10 most satisfied customers
+	st.subheader('Top 10 most satisfied customers')
+	sample_size = st.slider('Sample Size', 10, int(len(experience_data)))
+	st.dataframe(experience_data.sort_values('Satisfaction Score',axis=0, ascending=False, inplace=False)[:sample_size])
+	# screenshot of db fetch
 
 # model prediction testing
 def ai_function():
@@ -21,7 +82,7 @@ def ai_function():
 with st.sidebar:
 	selected = option_menu(
 			menu_title = 'Menu',
-			options = ['Project Description','Analysis', 'AI', 'About Me']
+			options = ['Analysis', 'AI', 'About Me']
 		)
 
 if selected == 'Project Description':
